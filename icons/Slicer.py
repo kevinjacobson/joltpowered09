eacae24339ecf3bbb96e7fb738124b7f9d43f3d3
Kevin Jacobson <kj@arebelong.(none)>
2009-05-09 00:12:57 -0400

    AWesomeness

--- icons/Slicer.py
+++ icons/Slicer.py
@@ -15,16 +15,29 @@ for i in range(5):
 	for j in range(11):
 		count += 1
 		rect = iWidth*j,iHeight*i,iWidth*(j+1),iHeight*(i+1)
-		if(count>=2):
+		if(count>=2 and count < 54):
+			num = (count-2)%13+1
+			if(num==1):
+				val = "ace"
+			elif(num==11):
+				val = "jack"
+			elif(num==12):
+				val = "queen"
+			elif(num==13):
+				val = "king"
+			else:
+				val = str(num)
+
+					 
 			if((count-2)/13==0):
-				im.crop(rect).save("spades/"+str((count-2)%13+1)+".jpg")
+				im.crop(rect).save("spades/"+val+".jpg")
 			elif((count-2)/13==1):
-				im.crop(rect).save("hearts/"+str((count-2)%13+1)+".jpg")
+				im.crop(rect).save("hearts/"+val+".jpg")
 			elif((count-2)/13==2):
-				im.crop(rect).save("clubs/"+str((count-2)%13+1)+".jpg")
+				im.crop(rect).save("clubs/"+ val+".jpg")
 			else:	
-				im.crop(rect).save("diamonds/"+str((count-2)%13+1)+".jpg")		
+				im.crop(rect).save("diamonds/"+val+".jpg")		
 
 				
-
+print count
 	
