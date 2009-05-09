674493882d396ac78b7dd3262d78fd1419a6e798
Kevin Jacobson <kj@arebelong.(none)>
2009-05-09 03:27:29 -0400

    add slices

--- icons/Slicer.py
+++ icons/Slicer.py
@@ -15,6 +15,7 @@ for i in range(5):
 	for j in range(11):
 		count += 1
 		rect = iWidth*j+20,iHeight*i+20,iWidth*(j+1)-20,iHeight*(i+1)-20
+		rect2 = rect[0],rect[1],rect[0]+100,rect[3]
 		if(count>=2 and count < 54):
 			num = (count-2)%13+1
 			if(num==1):
@@ -28,17 +29,24 @@ for i in range(5):
 			else:
 				val = str(num)
 
-			crop = im.crop(rect)			 
+			crop = im.crop(rect)		
+			crop2 = im.crop(rect2)	 
 			w,h = crop.size
+			w2,h2 = crop2.size
 			crop = crop.resize((w/4,h/4))
+			crop2 = crop2.resize((w2/4,h2/4))
 			if((count-2)/13==0):
 				crop.save("spades/"+val+".jpg")
+				crop2.save("spades/"+val+"S.jpg")
 			elif((count-2)/13==1):
 				crop.save("hearts/"+val+".jpg")
+				crop2.save("hearts/"+val+"S.jpg")
 			elif((count-2)/13==2):
 				crop.save("clubs/"+ val+".jpg")
+				crop2.save("clubs/"+val+"S.jpg")
 			else:	
-				crop.save("diamonds/"+val+".jpg")		
+				crop.save("diamonds/"+val+".jpg")
+				crop2.save("diamonds/"+val+"S.jpg")		
 
 				
 print count
