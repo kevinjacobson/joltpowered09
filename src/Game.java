93cf4a869d0fd2896de0d7e6182b330c3fd736a9
ROBERT JAMES SCHRIVER <rjs7540@vermont.(none)>
2009-05-09 01:57:22 -0400

    Added probablity to Game

--- src/Game.java
+++ src/Game.java
@@ -7,12 +7,17 @@ public class Game{
     static int currPlayerIndex;
     static Hand currHand;
     static int currHandIndex;
+    //int numDecks, numPlayers;
+    int numDiscardedDecks = 0;
     
     public Game(int numPlayers , int numDecks){
         startGame(numPlayers , numDecks);
     }
     
     public void startGame(int numPlayers , int numDecks){
+        deckList = new ArrayList<Deck>();
+        playerList = new ArrayList<Player>();
+        
         for(int i = 0; i < numDecks; i ++){
             Deck d = new Deck();
             d.shuffle();
@@ -56,8 +61,10 @@ public class Game{
     }
 
     public void hit(){
-        if(deckList.get(0).getNumCards() <= 0)
+        if(deckList.get(0).getNumCards() <= 0){
+            numDiscardedDecks ++;
             deckList.remove(0);
+        }
 
         currHand.addCard(deckList.get(0).draw());
         
@@ -68,8 +75,10 @@ public class Game{
     public void doubleDown(){
         if(currPlayer.getLargestHand().size() == 2){
             currPlayer.bet(currPlayer.getBet() * 2);
-            if(deckList.get(0).getNumCards() <= 0)
+            if(deckList.get(0).getNumCards() <= 0){
+                numDiscardedDecks ++;
                 deckList.remove(0);
+            }
             Card currCard = deckList.get(0).draw();
             currCard.faceDown();
             currHand.addCard(currCard);
@@ -87,10 +96,58 @@ public class Game{
                 currPlayer.getHands().get(currPlayer.getHands().size() - 1).addCard(currHand.getCards().remove(0));
             }
         }
+    }   
+    public int getProbablity(int val){
+        return getProbablity(val , currPlayer);
     }
 
-    public Player getCurrPlayer(){}
+    public int getProbablity(int val , Player p){
         
+        int numVal = getNumCardsVal(val , p);
+        
+        if(val == 10)
+            return (16 * deckList.size() - numVal) / 
+                (52 * deckList.size() - (52 * numDiscardedDecks + deckList.get(0).getNumRemovedCards()));
+        else  return (4 * deckList.size() - numVal) / 
+                  (52 * deckList.size() - (52 * numDiscardedDecks + deckList.get(0).getNumRemovedCards()));
+    }
+    
+    public int probabilityOfBusting(){
+        return probabilityOfBusting(currPlayer);
+    }
+
+    public int probabilityOfBusting(Player p){
+        int prob = 0;
+        for(int i = 0; i < p.getHands().size(); i ++){
+            Hand currHand = p.getHands().get(i);
+            for(int j = 21 - currHand.leastValue(); j <= 11; j++)
+                prob += getProbablity(j);
+        }
+        return prob;
+    }
+
+    //public int probablityOfGreaterHand(Player curr , Player to){
+        
+        
+    public int getNumCardsVal(int val , Player p){
+        int numVal = 0;
+        for(int i = 0; i < playerList.size(); i ++){
+            ArrayList<Hand> currHands = playerList.get(i).getHands();
+            for(int j = 0; j < currHands.size(); j ++){
+                ArrayList<Card> currHand = currHands.get(j).getCards();
+                for(int h = 0; h < currHand.size(); h ++){
+                    if(val == 1){
+                        if(currHand.get(h).getValue() == 11 && ( !currHand.get(h).isFaceDown() ||  p.equals(playerList.get(i))))
+                            numVal ++;
+                    }
+                else if(currHand.get(h).getValue() == val && (!currHand.get(h).isFaceDown() || p.equals(playerList.get(i))))
+                    numVal ++;
+                }
+            }
+        }
+        return numVal;
+    }
+            
 }
     
     
